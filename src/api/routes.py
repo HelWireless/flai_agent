cc7299f2702540572e3a3be0c2ab9dedb3095a5e
cody <cody.he@nike.com>
2025-03-08 19:33:12 +0800

    add more character prompt

--- src/api/routes.py
+++ src/api/routes.py
@@ -23,7 +23,7 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from functools import partial
 from datetime import datetime
-from prompts.generate_prompts import *
+from src.api.prompts.generate_prompts import *
 
 router = APIRouter(
     prefix="/pillow",
