208484aba519029cc7a3b1ee10ea2a0fc24a71fb
cody <cody.he@nike.com>
2025-05-14 22:39:42 +0800

    add unhandled_exception_handler handler

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def create_app() -> FastAPI:
     return app
 
 app = create_app()
-app.include_router(router)
+
 
 async def set_body(request: Request):
     receive_ = await request._receive()
@@ -44,6 +44,7 @@ async def unhandled_exception_handler(request: Request, exc: Exception):
         content={"detail": "服务器内部错误，请稍后再试。"},
     )
 
+app.include_router(router)
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
