bf1ddfa1cfc4df3453d479f6a2cc76ca9acbd903
cody <cody.he@nike.com>
2025-05-14 22:35:22 +0800

    add error handler

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import uvicorn
 from src.api.routes import router
-
+from custom_logger import *
 def create_app() -> FastAPI:
     app = FastAPI(title="Pillow Talk", debug=False)
     app.add_middleware(
@@ -28,7 +28,7 @@ async def set_body(request: Request):
 async def http_exception_handler(request: Request, exc: Exception):
     await set_body(request)
     request_text = (await request.body()).decode("utf-8")[:100]  # 安全读取 body 并限制长度
-    app.logger.error(f'请求发生异常，记录request的请求体如下:{request_text}')
+    custom_logger.error(f'请求发生异常，记录request的请求体如下:{request_text}')
     return JSONResponse(
         status_code=exc.status_code if isinstance(exc, HTTPException) else 500,
         content={"detail": exc.detail}
